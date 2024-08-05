43c2554d3a7425c61a09dc7aea857596c73cac60
Salma Halhoul <halhoulsa00@gmail.com>
2024-08-05 11:33:54 +0000

    Connect to a MongoDB Database Using Node.js

--- server.js
+++ server.js
@@ -1,14 +1,22 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
+const port = 3000;
+const mongoose = require("mongoose");
 
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   // send simple code
   // res.send('<h1>Hello World!</h1>')
   // send file html
-  res.sendFile('./views/home.html', {root: __dirname})
-})
+  res.sendFile("./views/home.html", { root: __dirname });
+});
 
-app.listen(port, () => {
-  console.log(`http://localhost:${port}/`)
-})
+mongoose
+  .connect("mongodb+srv://halhoulsa00:DBC5gt05EDeSYDWG@cluster0.royeevr.mongodb.net/all-data?retryWrites=true&w=majority&appName=Cluster0")
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`http://localhost:${port}/`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
